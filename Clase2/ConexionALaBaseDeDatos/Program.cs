0bb39b894ab812147d6e2d2abd6e7f67f6694556
calebor0444 <calebor04@gmail.com>
2026-01-23 19:07:02 -0600

    proyecto de ejemplo conexion a bd

---
+++ Clase2/ConexionALaBaseDeDatos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConexionALaBaseDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using var elContextoBd = ConexionABD.ConfigurarLaConexion();
+
+                var laCantidadDeArtistas = elContextoBd.Artists.Count();
+                Console.WriteLine($"Artistas en la base: {laCantidadDeArtistas}");
+
+                var primerArtista = elContextoBd.Artists.First();
+                Console.WriteLine($"Primer artista: {primerArtista.Name}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al conectar a la base de datos: {ex.Message}");
+                Environment.Exit(1);
+            }
+        }
+
+       
+    }
+}
